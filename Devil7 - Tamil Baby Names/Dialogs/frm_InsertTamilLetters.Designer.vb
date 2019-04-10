03b16925cb4e38d0e794affcf9e430505b4d62ac
Devil7DK <dineshthangavel47@gmail.com>
2019-04-11 00:24:41 +0530

    Add Dialog to Insert Tamil Unicode Letters

---
+++ Devil7 - Tamil Baby Names/Dialogs/frm_InsertTamilLetters.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_InsertTamilLetters
+    Inherits XtraFormTemp
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_InsertTamilLetters))
+        Me.grp_Letters = New DevExpress.XtraEditors.GroupControl()
+        Me.lst_Letters = New DevExpress.XtraEditors.CheckedListBoxControl()
+        Me.btn_Insert = New DevExpress.XtraEditors.SimpleButton()
+        CType(Me.grp_Letters, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grp_Letters.SuspendLayout()
+        CType(Me.lst_Letters, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'grp_Letters
+        '
+        Me.grp_Letters.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.grp_Letters.Controls.Add(Me.lst_Letters)
+        Me.grp_Letters.Location = New System.Drawing.Point(12, 12)
+        Me.grp_Letters.Name = "grp_Letters"
+        Me.grp_Letters.Size = New System.Drawing.Size(232, 217)
+        Me.grp_Letters.TabIndex = 0
+        Me.grp_Letters.Text = "Select Letters"
+        '
+        'lst_Letters
+        '
+        Me.lst_Letters.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lst_Letters.Location = New System.Drawing.Point(2, 20)
+        Me.lst_Letters.Name = "lst_Letters"
+        Me.lst_Letters.Size = New System.Drawing.Size(228, 195)
+        Me.lst_Letters.SortOrder = System.Windows.Forms.SortOrder.Ascending
+        Me.lst_Letters.TabIndex = 0
+        '
+        'btn_Insert
+        '
+        Me.btn_Insert.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_Insert.Location = New System.Drawing.Point(169, 235)
+        Me.btn_Insert.Name = "btn_Insert"
+        Me.btn_Insert.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Insert.TabIndex = 1
+        Me.btn_Insert.Text = "Insert"
+        '
+        'frm_InsertTamilLetters
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(256, 270)
+        Me.Controls.Add(Me.btn_Insert)
+        Me.Controls.Add(Me.grp_Letters)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frm_InsertTamilLetters"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Insert Tamil Letters"
+        CType(Me.grp_Letters, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grp_Letters.ResumeLayout(False)
+        CType(Me.lst_Letters, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents grp_Letters As DevExpress.XtraEditors.GroupControl
+    Friend WithEvents btn_Insert As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents lst_Letters As DevExpress.XtraEditors.CheckedListBoxControl
+End Class
---
+++ Devil7 - Tamil Baby Names/Dialogs/frm_InsertTamilLetters.vb
@@ -0,0 +1,60 @@
+﻿'=========================================================================='
+'                                                                          '
+'                    (C) Copyright 2018 Devil7 Softwares.                  '
+'                                                                          '
+' Licensed under the Apache License, Version 2.0 (the "License");          '
+' you may not use this file except in compliance with the License.         '
+' You may obtain a copy of the License at                                  '
+'                                                                          '
+'                http://www.apache.org/licenses/LICENSE-2.0                '
+'                                                                          '
+' Unless required by applicable law or agreed to in writing, software      '
+' distributed under the License is distributed on an "AS IS" BASIS,        '
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. '
+' See the License for the specific language governing permissions and      '
+' limitations under the License.                                           '
+'                                                                          '
+' Contributors :                                                           '
+'     Dineshkumar T                                                        '
+'=========================================================================='
+
+Public Class frm_InsertTamilLetters
+
+#Region "Properties"
+    Property Letters As List(Of String)
+#End Region
+
+#Region "Form Events"
+    Private Sub frm_InsertTamilLetters_Load(sender As Object, e As EventArgs) Handles Me.Load
+        For Each i As Objects.Name In DatabaseIO.GetNames(True)
+            If Not lst_Letters.Items.Contains(i.FirstLetter.Trim) AndAlso i.FirstLetter.Trim <> "" Then lst_Letters.Items.Add(i.FirstLetter.Trim)
+        Next
+        If Letters IsNot Nothing Then
+            For Each i As String In Letters
+                Dim index = lst_Letters.Items.IndexOf(i.Trim)
+                If index >= 0 Then
+                    lst_Letters.Items(index).CheckState = CheckState.Checked
+                End If
+            Next
+        End If
+    End Sub
+#End Region
+
+#Region "Button Events"
+    Private Sub btn_Insert_Click(sender As Object, e As EventArgs) Handles btn_Insert.Click
+        If Letters Is Nothing Then
+            Letters = New List(Of String)
+            For Each i As DevExpress.XtraEditors.Controls.CheckedListBoxItem In lst_Letters.CheckedItems
+                Letters.Add(i.Value)
+            Next
+        Else
+            For Each i As DevExpress.XtraEditors.Controls.CheckedListBoxItem In lst_Letters.CheckedItems
+                If Not Letters.Contains(i.Value) Then Letters.Add(i.Value)
+            Next
+        End If
+        DialogResult = DialogResult.OK
+        Close()
+    End Sub
+#End Region
+
+End Class
